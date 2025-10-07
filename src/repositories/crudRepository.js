7c099786873d71ba9c7f00863b843bd1a59056a3
aakashmaity <aakashmaity57@gmail.com>
2025-10-07 22:20:06 +0530

    Added CRUD repository

---
+++ src/repositories/crudRepository.js
@@ -0,0 +1,24 @@
+export default function crudRepository(model) {
+    return {
+        create: async function (data) {
+            const newDoc = await model.create(data);
+            return newDoc;
+        },
+        getAll: async function () {
+            const allDocs = await model.find();
+            return allDocs;
+        },
+        getById: async function (id) {
+            const doc = await model.findById(id);
+            return doc;
+        },
+        delete: async function (id) {
+            const deletedDoc = await model.findByIdAndDelete(id);
+            return deletedDoc;
+        },
+        update: async function (id, updateData) {
+            const updatedDoc = await model.findByIdAndUpdate(id, updateData, { new: true });
+            return updatedDoc;
+        }
+    }
+}
---
+++ src/repositories/userRepository.js
@@ -0,0 +1,16 @@
+import User from "../schema/user.js";
+import crudRepository from "./crudRepository.js";
+
+const userRepository = {
+    ...crudRepository(User),
+    getUserByEmail: async function(email) {
+        const user = await User.findOne({ email });
+        return user;
+    },
+    getUserByUsername: async function(username) {
+        const user = await User.findOne({ username });
+        return user;
+    }
+}
+
+export default userRepository;
